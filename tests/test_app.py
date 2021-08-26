89902a01a646e27f8fc22d8fcf7371ec5ce94916
Alex M <alex@mosstech.io>
2021-08-26 21:13:01 +0100

    Switch to magic __main__ to make docker setup easier

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 import blueprint.app
 
 
-def test_app(capsys, caplog, example_fixture):
+def test_app(caplog, example_fixture):
     first_name = example_fixture
     blueprint.app.app(first_name)
     assert "Hello Alex" in caplog.text
--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-import blueprint.main
-
-
-def test_main(mocker):
-    mocker.patch('blueprint.main.app')
-    blueprint.main.app()
